0001412ca85e1162f10ec4a38946f12583e447a9
AvazAlimov <avazalimov95@mail.ru>
2019-07-13 20:28:40 +0500

    Permissions added

--- src/views/shipments/Main.vue
+++ src/views/shipments/Main.vue
@@ -5,6 +5,7 @@
       v-for="(tab, index) in tabs" :key="index"
       flat color="tertiary"
       :to="tab.to"
+      v-if="tab.visible"
     ) {{ tab.name }}
     v-flex.mt-2(xs12)
       router-view
@@ -13,25 +14,31 @@
 <script>
 export default {
   name: 'ShipmentsMain',
-  data: () => ({
-    tabs: [
-      {
-        to: { name: 'mysales' },
-        name: 'Мои отгрузки',
-      },
-      {
-        to: { name: 'salesinformation' },
-        name: 'Отгрузки',
-      },
-      {
-        to: { name: 'pendingsales' },
-        name: 'На согласовании',
-      },
-      {
-        to: { name: 'approvedsales' },
-        name: 'Согласованные',
-      },
-    ],
-  }),
+  data() {
+    return {
+      tabs: [
+        {
+          to: { name: 'mysales' },
+          name: 'Мои отгрузки',
+          visible: this.$hasPermission(this.$permissions.SHOW_SALES_MY_SALES.name),
+        },
+        {
+          to: { name: 'salesinformation' },
+          name: 'Отгрузки',
+          visible: this.$hasPermission(this.$permissions.SHOW_SALES_INFORMATION.name),
+        },
+        {
+          to: { name: 'pendingsales' },
+          name: 'На согласовании',
+          visible: this.$hasPermission(this.$permissions.SHOW_SALES_PENDING.name),
+        },
+        {
+          to: { name: 'approvedsales' },
+          name: 'Согласованные',
+          visible: this.$hasPermission(this.$permissions.SHOW_SALES_APPROVED.name),
+        },
+      ],
+    };
+  },
 };
 </script>
