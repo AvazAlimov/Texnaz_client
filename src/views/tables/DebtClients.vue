7124b2828e23f6133e5ba8b684b658f3328b8ff8
Makhmudjon <maxmudinadullayev98@gmail.com>
2019-07-20 01:45:38 +0500

    Client balance table configured.

--- src/views/tables/DebtClients.vue
+++ src/views/tables/DebtClients.vue
@@ -12,6 +12,7 @@
 
 <script>
 import Sale from '@/services/Sale';
+import Payment from '@/services/Payment';
 import Configuration from '@/services/Configuration';
 
 export default {
@@ -61,18 +62,29 @@ export default {
         Sale.getAll(),
         Configuration.getExchangeRate(),
         Configuration.getOfficialRate(),
+        Payment.getAll(),
       ]).then((result) => {
-        result[0].forEach((el) => {
+        // Main task: Remove objects from Payments if it exist in Sale then bind two array
+        // and loop it to display on tabel
+        let holder = result[3];
+        result[0].forEach((element) => {
+          // Removing, objects if the client in it exist in Sale also, from Payments array
+          holder = holder.filter(el => element.client.id !== el.client.id);
+        });
+        // Connecting two types of Objects, Sale and Payment
+        result[0].concat(holder).forEach((el) => {
           this.users.push({
             icc: el.client.icc,
             name: el.client.name,
-            saleDate: el.createdAt,
-            salePrice: this.$getTotalPrice(el, result[1].value, result[2].value),
+            // To diffirentiate which object Sale or Payment .ratio prop added
+            saleDate: el.ratio ? '-' : el.createdAt,
+            salePrice: el.ratio ? '-' : this.$getTotalPrice(el, result[1].value, result[2].value),
+            // In order to get last date and price of payment .reduce function used
             paymentDate: el.client.payments.reduce((first, next) => next.createdAt, '-'),
             paymentPrice: el.client.payments.reduce((first, next) => next.sum, '-'),
             manager: el.manager.name,
             debt: el.client.payments.map(payment => payment.sum).reduce((a, b) => a + b, 0)
-              - this.$getTotalPrice(el, result[1].value, result[2].value),
+               - (el.ratio ? 0 : this.$getTotalPrice(el, result[1].value, result[2].value)),
           });
         });
       });
