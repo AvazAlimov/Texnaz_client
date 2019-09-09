ca7c994f13273623b28a8d1feae8e2283a46bc77
maxmudinadullayev98@gmail.com <maxmudinadullayev98@gmail.com>
2019-09-10 04:11:34 +0500

    Sales logic implemented

--- src/utils/Sale.js
+++ src/utils/Sale.js
@@ -1,12 +1,19 @@
 import { calculate } from './Payment';
 import Client from '@/services/Client';
 
-export default (clientId, rate) => {
+export default (clientId, salePrice, rate) => new Promise((res, rej) => {
   Client.get(clientId)
     .then((client) => {
       if (client.balance > 0) {
         calculate(clientId, 0, client.balance, rate);
+        res();
+      }
+      if (client.balance < salePrice) {
+        Client.addBalance(clientId,
+          0 - (Number.parseFloat(salePrice) / rate))
+          .then(() => { res(); })
+          .catch(err => rej(err));
       }
     })
-    .catch(error => console.error(error));
-};
+    .catch(error => rej(error));
+});
