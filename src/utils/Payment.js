40840834dec6d6aee869370748121caa2dd07679
maxmudinadullayev98@gmail.com <maxmudinadullayev98@gmail.com>
2019-09-11 19:27:55 +0500

    Payment logic modified for client balance

--- src/utils/Payment.js
+++ src/utils/Payment.js
@@ -34,13 +34,17 @@ export function currencyRespectPayment(payment, paymentType, saleType, rate) {
  * @param {Payment value} value
  * @param {Rate for converting} rate
  */
-export async function calculate(clientId, type, value, rate) {
-  await Sale.getByClient(clientId)
+export const calculate = (clientId, type, value, rate) => new Promise((resolve, reject) => {
+  // UPDATE client balance
+  Client.addBalance(clientId, type === 0 ? value : value / rate);
+
+  Sale.getByClient(clientId)
     .then(async (sales) => {
-      let paymentValue = Number.parseFloat(value) || 0;
+      const paymentValue = Number.parseFloat(value) || 0;
+
       await sales.sort((a, b) => (a.createdAt > b.createdAt ? 1 : -1)).forEach(async (sale) => {
         if (paymentValue > 0) {
-        //  payment respect currency
+          //  payment respect currency
           let payment = paymentRespectCurrency(paymentValue, type, sale.type, rate);
           const tasks = [];
           // payment for each sale product
@@ -50,16 +54,16 @@ export async function calculate(clientId, type, value, rate) {
               // debtPrice > 0 means product is not paid fully
               if (saleItem.debtPrice > 0) {
                 if (saleItem.debtPrice > (Number.parseFloat(saleItem.paidPrice)
-                    + Number.parseFloat(payment))) {
-                // UPDATE paidPrice with (saleItem.paidPrice + payment)
+                  + Number.parseFloat(payment))) {
+                  // UPDATE paidPrice with (saleItem.paidPrice + payment)
                   tasks.push(Sale.updateSaleItem(saleItem.id, {
                     paidPrice: Number.parseFloat(saleItem.paidPrice) + Number.parseFloat(payment),
                   }));
                   payment = 0;
                 } else if (payment !== 0) {
-                  // UPDATE debtPrice and paidPrice are equal now
+                // UPDATE debtPrice and paidPrice are equal now
                   payment = (Number.parseFloat(saleItem.paidPrice)
-                      + Number.parseFloat(payment)) - Number.parseFloat(saleItem.debtPrice);
+                    + Number.parseFloat(payment)) - Number.parseFloat(saleItem.debtPrice);
                   tasks.push(Sale.updateSaleItem(saleItem.id, {
                     paidPrice: saleItem.debtPrice,
                     debtPrice: 0,
@@ -69,29 +73,21 @@ export async function calculate(clientId, type, value, rate) {
             }
           });
 
-          // check is there residue of the payment
-          paymentValue = payment > 0 ? currencyRespectPayment(payment, type, sale.type, rate) : 0;
-
-          await Promise.all(tasks)
-            .then(() => {
-            // check whether all products payed, if yeah then update sale status to Closed
-              Sale.getByClient(clientId).then((newSales) => {
+          Promise.all(tasks)
+            .then(async () => {
+              // check whether all products payed, if yeah then update sale status to Closed
+              await Sale.getByClient(clientId).then((newSales) => {
                 newSales.forEach(async (newSale) => {
-                  // eslint-disable-next-line max-len
                   if (!newSale.items.filter(({ debtPrice }) => debtPrice !== 0).length) {
-                  // UPDATE sale status closed
+                    // UPDATE sale status closed
                     await Sale.close(newSale.id);
                   }
                 });
               });
             })
-            .catch(err => console.error(err));
+            .catch(err => reject(err));
         }
       });
-
-      if (paymentValue > 0) {
-        // UPDATE client balance
-        Client.addBalance(clientId, type === 0 ? paymentValue : paymentValue * rate);
-      }
-    }).catch((error) => { console.error(error); });
-}
+      resolve();
+    }).catch((error) => { reject(error); });
+});
