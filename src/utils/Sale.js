f43ab5f1ba512fa38364b355384c4ceb997f1382
Makhmudjon <maxmudinadullayev98@gmail.com>
2019-11-26 01:34:45 +0500

    Steps for client balance addition while shipment is changed

--- src/utils/Sale.js
+++ src/utils/Sale.js
@@ -5,7 +5,7 @@ import Client from '@/services/Client';
 export default (clientId, salePrice, type, rate) => new Promise((res, rej) => {
   Client.get(clientId)
     .then((client) => {
-      Client.addBalance(clientId, 0 - (type === 1 ? (salePrice / rate) : salePrice))
+      Client.addBalance(clientId, 0 - ((type === 1 || type === 4) ? (salePrice / rate) : salePrice))
         .then(() => {
           calculate(clientId, 0, client.balance, rate)
             .then(() => Sale.check(clientId)
