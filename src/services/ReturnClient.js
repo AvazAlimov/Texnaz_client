297c91dc60c5300d4b876b7ee0f676c753b341ab
Makhmudjon <maxmudinadullayev98@gmail.com>
2019-12-17 13:30:12 +0500

    Get by return id method added

--- src/services/ReturnClient.js
+++ src/services/ReturnClient.js
@@ -2,5 +2,6 @@ import Api, { execute } from './Api';
 
 export default {
   getAll: () => execute(Api().get('returnclients')),
+  get: id => execute(Api().get(`returnclients/${id}`)),
   create: returnItem => execute(Api().post('returnclients', returnItem)),
 };
