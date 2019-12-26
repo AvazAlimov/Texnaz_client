9ee1d52c2cd8e18d624d28ef20126f9e0825047f
Makhmudjon <maxmudinadullayev98@gmail.com>
2019-12-26 16:29:16 +0500

    SalesHistory created

--- src/router.js
+++ src/router.js
@@ -497,7 +497,11 @@ const router = new Router({
                     },
                   ],
                 },
-
+                {
+                  path: 'history',
+                  name: 'returnHistory',
+                  component: () => import('./views/warehouses/SalesHistory.vue'),
+                }
               ],
             },
             {
---
+++ src/views/warehouses/SalesHistory.vue
@@ -0,0 +1,12 @@
+<template lang="pug">
+  v-layout(row wrap align-center)
+    v-flex(xs12)
+      .border.white
+        .title Hello World
+</template>
+
+<script>
+export default {
+  name: 'SalesHistory',
+};
+</script>
